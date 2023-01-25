302dc9b4a91c4fb8639a5a1ac675bdf6f71e7f83
FahCodes <muhammadpahmi8@gmail.com>
2023-01-25 16:24:35 +0700

    Adding logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,4 +34,12 @@ class AuthController extends Controller
         Session::flash('message', 'Login Gagal / Tidak Ditemukan Data');
         return redirect('login');
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('login');
+    }
 }
