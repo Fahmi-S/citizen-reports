86816e38f094a216c9e4658775e655a1e11fa6b1
FahCodes <muhammadpahmi8@gmail.com>
2023-02-07 12:53:54 +0700

    fixed some issue

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Report;
 use App\Models\Petugas;
+use App\Models\Masyarakat;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        // Menghitung jumlah Petugas
         $petugasCount = Petugas::count();
+        // Menghitung jumlah Masyarakat
+        $masyarakatCount = Masyarakat::count();
+        // Menghitung jumlah Petugas + jumlah Masyarakat
+        $totaluser = $petugasCount + $masyarakatCount;
+        // Menghitung jumlah Pengaduan yang sedang proses
         $processreportCount  = Report::where('status', 'proses')->count();
+        // Menghitung jumlah pengaduan yang sudah selesai
         $finishedreportCount = Report::where('status', 'selesai')->count();
+        // Menghitung jumlah seluruh pengaduan
         $allreportCount = Report::count();
-        return view('dashboard', ['petugas_count' => $petugasCount, 'processreport_count' => $processreportCount, 'allreport_count' => $allreportCount, 'finishedreport_count' => $finishedreportCount]);
+        // Memasukan variable kedalam blade
+        return view('dashboard', ['total_user' => $totaluser, 'processreport_count' => $processreportCount, 'allreport_count' => $allreportCount, 'finishedreport_count' => $finishedreportCount]);
     }
 }
